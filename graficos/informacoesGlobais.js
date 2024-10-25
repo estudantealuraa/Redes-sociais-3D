806b87887d5df12db408e35a5fa10d2a93582601
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-10-25 16:08:47 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,12 @@
 const url='https://github.com/guilhermeonrails/api/blob/main/dados-globais.json'
+async function visualizarInformacoesGlobais(){
+  const res= await fetch(url)
+  const dados= await res.json()
+  console.log(dados);
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container_texto')
+
+  paragrafo.innerHTML=
+}
 
 console.log(url);
